0e0fa31d097ed68d8917196235b819de5ab888de
Mint <mint29006@gmail.com>
2021-06-21 15:51:10 +0700

    UPDATE API front-back in recordvaccine.dart

--- lib/recordvaccine.dart
+++ lib/recordvaccine.dart
@@ -1,162 +1,235 @@
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'successrecord.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RecordVacine extends StatefulWidget {
   @override
   _RecordVacineState createState() => _RecordVacineState();
 }
 
+class Vaccines {
+  int vaccine_id;
+  String vac_nameen, vac_nameth, detail;
+  Vaccines(this.vaccine_id, this.vac_nameen, this.vac_nameth, this.detail);
+}
+
 class _RecordVacineState extends State<RecordVacine> {
-  String v1 = 'มือปากเท้าเปื่อย';
-  String v2 = 'ไข้ขา';
-  String v3 = 'คอบวม';
-  String v4 = 'กาลี';
+  getVaccines() async {
+    var response = await http.get(Uri.http('10.0.2.2:3000', 'vaccines'));
+    var data = json.decode(response.body);
+    List<Vaccines> vaccines = [];
+    for (var u in data) {
+      Vaccines vaccine = Vaccines(
+          u['vaccine_id'], u['vac_nameen'], u['vac_nameth'], u['detail']);
+      vaccines.add(vaccine);
+    }
+    print(vaccines.length);
+    return vaccines;
+  }
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    getVaccines();
+  }
+
+  int _selectIndex = 0;
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
 
   DateTime _dateTime;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('บันทึกการฉีดวัคซีน'),
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Color(0xff59aca9),
-      ),
-      body: Container(
-          child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            child: DropdownSearch<String>(
-                mode: Mode.MENU,
-                showSelectedItem: true,
-                items: [v1, v2, v3, v4],
-                label: "ชื่อวัคซีน",
-                hint: "country in menu mode",
-                popupItemDisabled: (String s) => s.startsWith('I'),
-                onChanged: print,
-                selectedItem: v1),
-            padding: const EdgeInsets.all(20.0),
-          ),
-          Container(
-            alignment: Alignment.topLeft,
-            margin: EdgeInsets.all(0),
-            padding: const EdgeInsets.fromLTRB(20, 10, 0, 10),
-            child:
-                Text('วันที่', style: TextStyle(fontWeight: FontWeight.bold)),
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 20, 0, 20),
-                child: Text(
-                  _dateTime == null ? 'yyyy/mm/dd' : _dateTime.toString(),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.calendar_today_sharp,
-                    color: Colors.blueGrey,
-                  ),
-                  onPressed: () {
-                    showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1970),
-                      lastDate: DateTime(2022),
-                    ).then((date) {
-                      setState(() {
-                        _dateTime = date;
-                      });
-                    });
-                  },
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'รายละเอียดอื่นๆ',
-                  fillColor: Colors.blueGrey,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blueGrey, width: 2),
-                  )),
-              onChanged: (String name) {},
+        appBar: AppBar(
+          title: Text('บันทึกการฉีดวัคซีน'),
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                  margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // ignore: deprecated_member_use
-                      RaisedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        color: Colors.blueGrey[50],
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(39))),
-                        child: Text(
-                          'ยกเลิก',
-                          style: TextStyle(
-                              color: Color(0xffd6786e),
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14),
+          backgroundColor: Color(0xff59aca9),
+        ),
+        body: Container(
+            child: FutureBuilder(
+          future: getVaccines(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Text('Loading...'),
+                ),
+              );
+            } else
+              return Container(
+                child: Column(
+                  children: [
+                    Container(
+                        // child: DropdownButton<String>(
+                        //     hint: Text('${snapshot.data[0].vac_nameth}'),
+                        //     value: '${snapshot.data[0].vac_nameth}',
+                        //     onChanged: (newValue) {
+                        //       setState(() {
+                        //         snapshot.data[0].vac_nameth = newValue;
+                        //       });
+                        //     },
+                        //     items: snapshot.data.map((vaccine) =>
+                        //       DropdownMenuItem<String>(
+                        //           child: Text(vaccine.vac_nameth),
+                        //           value: vaccine.vac_nameth)
+                        //     ).toList()
+                        //     )
+                        // child: ListView.builder(
+                        //     itemCount: snapshot.data.length,
+                        //     itemBuilder: (context, i) {
+                        //       return
+                        //       DropdownSearch <String> (
+                        //         mode: Mode.MENU,
+                        //         showSelectedItem: true,
+                        //         items: ['ไข้ขา'],
+                        //         label: "ชื่อวัคซีน",
+                        //         hint: "country in menu mode",
+                        //         popupItemDisabled: (String s) =>
+                        //         s.startsWith('I'),
+                        //         onChanged: print,
+                        //         selectedItem: 'ไข้ขา'
+                        //         );
+                            
+                              
+                             child: Container(
+                                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                                  child: DropdownSearch<String>(
+                                      mode: Mode.MENU,
+                                      showSelectedItem: true,
+                                      items: [snapshot.data[0].vac_nameth],
+                                      label: "ชื่อวัคซีน",
+                                      hint: "country in menu mode",
+                                      popupItemDisabled: (String s) =>
+                                          s.startsWith('I'),
+                                      onChanged: print,
+                                      selectedItem:
+                                          '${snapshot.data[0].vac_nameth}'),
+                                  padding: const EdgeInsets.all(20.0)
+                                  )
+                        //     }
+                        // )
+                         ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: EdgeInsets.all(0),
+                      padding: const EdgeInsets.fromLTRB(20, 10, 0, 10),
+                      child: Text('วันที่',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(30, 20, 0, 20),
+                          child: Text(
+                            _dateTime == null
+                                ? 'yyyy/mm/dd'
+                                : _dateTime.toString(),
+                          ),
                         ),
-                        padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                      )
-                    ],
-                  )),
-              Container(
-                  margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
-                  child: Column(
-                    children: [
-                      // ignore: deprecated_member_use
-
-                      RaisedButton(
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                            return SuccessRecord();
-                          }));
-                        },
-                        color: Color(0xff62b490),
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(39))),
-                        child: Text(
-                          'บันทึกข้อมูล',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w600,
-                              fontSize: 14),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 10, 0, 10),
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.calendar_today_sharp,
+                              color: Colors.blueGrey,
+                            ),
+                            onPressed: () {
+                              showDatePicker(
+                                      context: context,
+                                      initialDate: DateTime.now(),
+                                      firstDate: DateTime(1970),
+                                      lastDate: DateTime(2022))
+                                  .then((date) {
+                                setState(() {
+                                  _dateTime = date;
+                                });
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RaisedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  color: Colors.blueGrey[50],
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(39))),
+                                  child: Text(
+                                    'ยกเลิก',
+                                    style: TextStyle(
+                                        color: Color(0xffd6786e),
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14),
+                                  ),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(30, 10, 30, 10))
+                            ],
+                          ),
                         ),
-                        padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
-                      )
-                    ],
-                  )),
-            ],
-          ),
-        ],
-      )),
-    );
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+                          child: Column(
+                            children: [
+                              RaisedButton(
+                                  onPressed: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return SuccessRecord();
+                                    }));
+                                  },
+                                  color: Color(0xff62b490),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(39))),
+                                  child: Text(
+                                    'บันทึกข้อมูล',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14),
+                                  ),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(20, 12, 20, 12))
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+          },
+        )));
   }
 }
